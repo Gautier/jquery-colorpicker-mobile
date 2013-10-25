f8cafc324fdd0ce4db34084fafedb605b9fb4bde
Sebastian Roesner <sroesner-git@roesner-online.de>
2013-10-25 10:43:37 +0200

    Use parent div id as id if available, preventing duplicate colorpicker ids when having multiple colorpickers

--- js/colorpicker.js
+++ js/colorpicker.js
@@ -448,7 +448,7 @@
                     if (!$(this).data('colorpickerId')) {
                         var options = $.extend({}, opt);
                         options.origColor = opt.color;
-                        var id = 'colorpicker_' + parseInt(Math.random() * 1000);
+                        var id = 'colorpicker_' + (el.attr('id') || parseInt(Math.random() * 1000));
                         $(this).data('colorpickerId', id);
                         var cal = $(tpl).attr('id', id);
                         if (options.flat) {
